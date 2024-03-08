4d54d8a72a594fad58e6fa041ba786254daeb1e4
Kate Kozachenko <kottya1995@ukr.net>
2024-03-08 23:08:20 +0100

    tasks 15, 16 added

--- Auto-check6_2.py
+++ Auto-check6_2.py
@@ -69,4 +69,49 @@ class Contacts:
         Contacts.current_id += 1
 
     def get_contact_by_id(self, id):
-        pass
+        for rec in self.contacts:
+            if rec:
+                for key in rec:
+                    if rec[key] == id:
+                        return rec
+                    else: 
+                        continue
+            else:
+                return None
+            
+# Task 16/16
+class Contacts:
+    current_id = 1
+
+    def __init__(self):
+        self.contacts = []
+
+    def list_contacts(self):
+        return self.contacts
+
+    def add_contacts(self, name, phone, email, favorite):
+        self.contacts.append(
+            {
+                "id": Contacts.current_id,
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "favorite": favorite,
+            }
+        )
+        Contacts.current_id += 1
+
+    def get_contact_by_id(self, id):
+        result = list(filter(lambda contact: contact.get("id") == id, self.contacts))
+        return result[0] if len(result) > 0 else None
+
+    def remove_contacts(self, id):
+        for rec in self.contacts:
+            if rec:
+                for key in rec:
+                    if rec[key] == id:
+                        self.contacts.remove(rec)
+                    else: 
+                        continue
+            else:
+                pass
